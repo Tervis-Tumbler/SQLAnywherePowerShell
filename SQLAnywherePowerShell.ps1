8454dc5e1c61a512339dbbf9c0a80cd3837202b9
Chris Magnuson <hourglasssand@gmail.com>
2017-03-24 15:20:36 -0400

    Initial commit, still not finished but going to have to drop this for now

---
+++ SQLAnywherePowerShell.ps1
@@ -0,0 +1,29 @@
+﻿function Split-DBUnloadSQLFileIntoSections {
+    param (
+        $Path
+    )
+
+    $DBUnloadSQL = Get-Content -Path $Path
+    $SectionStartIndexNumbers = $DBUnloadSQL | foreach {
+        if ($_ -match "--   Create ") {
+            $DBUnloadSQL.IndexOf($_)
+        }
+    }
+    
+    $LastIndex = $SectionStartIndexNumbers.Count-1
+
+    $Sections =  @("") * $SectionStartIndexNumbers.Count
+    foreach ($Index in 0..$LastIndex) {
+        $StartOfSection = $SectionStartIndexNumbers[$Index]+2
+        $EndOfSection = if ($Index -eq $LastIndex) {
+            $DBUnloadSQL.Length
+        } else {
+            $SectionStartIndexNumbers[$Index+1]-2
+        }
+        #$StartOfSection
+        #$EndOfSection
+        #$DBUnloadSQL[$StartOfSection..$EndOfSection] | measure
+        $Sections[$Index] = $DBUnloadSQL[$StartOfSection..$EndOfSection]
+    }
+
+}
